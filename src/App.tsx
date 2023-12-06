8948da53dbb5b79d9da8719e02d2837955fec23a
Eno-Mario <pisellienomario@gmail.com>
2023-12-06 16:11:57 +0100

    component: created ratingReview

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import RatingReview, { Review } from "./product/RatingReview";
+
+const test: Review = {
+  img: "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
+  name: "Eno Mario",
+  userCountry: "Italy",
+  rating: 4,
+  ratingTitle: "buono",
+  commentDate: "01-01-2023",
+  comment: "Buon Prodotto",
+};
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <div className="dark">
+      <RatingReview {...test} />
+    </div>
+  );
 }
 
-export default App
+export default App;
