8ecaa9ee761c606b77f246abfe21f64217afd625
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-12-13 17:16:02 +0100

    component: Product-card

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import RatingReview, { Review } from "./product/RatingReview";
+import { Post, ProductCard } from "./shared/ProductCard";
+
 
 const test: Review = {
   img: "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
@@ -9,10 +11,16 @@ const test: Review = {
   commentDate: "01-01-2023",
   comment: "Buon Prodotto",
 };
+const test2: Post ={
+  img: "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
+  title: 'gino è bello',
+  description: 'lorem lorem'
+}
 
 function App() {
   return (
     <div className="dark">
+      <ProductCard {...test2} />
       <RatingReview {...test} />
     </div>
 
