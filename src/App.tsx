54512b165621ade68c9b2260103ae026e6961539
Stef533 <137123241+Stef533@users.noreply.github.com>
2023-12-13 13:05:29 +0100

    changet app import of footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import RatingReview, { Review } from "./product/RatingReview";
 
-import { Component } from "react";
+import FooterComponent from "./share/Footer";
 
 const test: Review = {
   img: "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="dark">
       <RatingReview {...test} />
-      <Component/>
+      <FooterComponent/>
     </div>
   );
 }
