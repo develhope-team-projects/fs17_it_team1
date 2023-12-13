31c33ab176a439ec3e08023392494df51cb2261f
Stef533 <137123241+Stef533@users.noreply.github.com>
2023-12-13 15:21:54 +0100

    create SitePath component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import RatingReview, { Review } from "./product/RatingReview";
-
+import { SitePathComponent } from "./shared/SitePath";
 const test: Review = {
   img: "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
   name: "Eno Mario",
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="dark">
       <RatingReview {...test} />
+      <SitePathComponent/>
     </div>
 
   );
