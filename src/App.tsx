1127436cbe3b62d1d3315cdd8f59597df16bd228
Adolfo-97 <adolfogomez@live.it>
2023-12-14 21:46:42 +0100

    Creazione componente Carousel Automatico

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import RatingReview, { Review } from "./product/RatingReview";
 import Header from "./shared/Header";
 
-
-import FooterComponent, {FooterType} from "./shared/Footer";
+import FooterComponent, { FooterType } from "./shared/Footer";
 
 import { Post, ProductCard } from "./shared/ProductCard";
 
-
 import { ButtonElements, NormalButton } from "./shared/NormalButton";
 import { SpecialButton, SpecialButtonElements } from "./shared/SpecialButton";
-
+import {
+  CarouselContent,
+  AutomaticCarousel,
+} from "./product/AutomaticCarousel";
 
 const test: Review = {
   img: "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
@@ -29,12 +30,11 @@ const test2: Post = {
 
 const prova: SpecialButtonElements = {
   content: "Special",
-
-}
+};
 
 const prova2: ButtonElements = {
   content: "prova",
-}
+};
 
 const footerTest: FooterType = {
   logo: "https://flowbite.com/docs/images/logo.svg",
@@ -54,13 +54,23 @@ const footerTest: FooterType = {
   thirdColumnElement3: "Indirizzi",
   thirdColumnElement4: "I miei avvisi",
   thirdColumnElement5: "Informazioni personali",
-}
+};
+const linkForTest: CarouselContent = {
+  Image1:
+    "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
+  Image2:
+    "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
+  Image3:
+    "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
+  Image4:
+    "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
+  Image5:
+    "https://cdn-7.motorsport.com/images/amp/0ZRabeN0/s1000/carlos-sainz-ferrari-charles-l.jpg",
+};
 
-function App(){
-
+function App() {
   return (
     <div className="dark">
-
       <Header />
 
       <ProductCard {...test2} />
@@ -69,12 +79,11 @@ function App(){
 
       <FooterComponent {...footerTest} />
 
-      <SpecialButton {...prova}/>
-      <NormalButton {...prova2}/>
-
+      <SpecialButton {...prova} />
+      <NormalButton {...prova2} />
+      <AutomaticCarousel {...linkForTest} />
     </div>
-
   );
-  }
+}
 
 export default App;
