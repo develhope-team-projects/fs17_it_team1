1c38d03176c6d2191f15006883519b70fc2ef7e2
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-12-26 02:30:48 +0100

    fix/ used swiper

--- src/product/Product.tsx
+++ src/product/Product.tsx
@@ -1,135 +1,169 @@
-'use client';
-/* -------------------FARE LO STILE,E AL CLICK DI UNA FOTO DEL CAROSELLO VERTICALE SI VEDA L'IMMAGINE NEL CAROSELLO GRANDE--------------------------- */
 
-import { Carousel, Footer } from 'flowbite-react';
+import { Footer } from 'flowbite-react';
 import { SitePathComponent } from '../shared/SitePath';
 import { SpecialButton } from '../shared/SpecialButton';
 import { useState } from 'react';
 import Header from '../shared/Header';
 import { Rating } from 'flowbite-react';
-
 import { Accordion } from 'flowbite-react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+
+// Import your custom styles
+import './product.css';
+
+// Import required modules
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import { Swiper as SwiperType } from 'swiper';
 
 type ProductProps = {
-    title: string;
-    // Altri campi del prodotto come necessario
-    immagini: { img1: string }[];
-    price: number;
+  title: string;
+  immagini: { img1: string }[];
+  price: number;
 };
 
 export function Product(props: ProductProps) {
-    const [img, setImg] = useState<string | undefined>(undefined);
-
-    const handleClick = (image: string) => {
-        setImg(image);
-    };
-
-    return (
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null);
+  
+  return (
+    <div>
+      <Header />
+      <SitePathComponent firstTitle="home" secondTitle="store" thirdTitle="product" />
+      <div>
         <div>
-            <Header />
-            <SitePathComponent firstTitle="home" secondTitle="store" thirdTitle="product" />
-            <div>
-                <div>
-                    {/* Carosello vertifcali delli immagini */}
-                    <div>
-                        {props.immagini.map((product, index) => (
-                            <button key={index} onClick={() => handleClick(product.img1)}>
-                                <img src={product.img1} alt={`carousel image ${index}`} />
-                            </button>
-                        ))}
-                    </div>
-                    {/* carsello grande */}
-                    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-                        <Carousel slide={false}>
-                            {props.immagini.map((product, index) => (
-                                <img key={index} src={product.img1} alt={`carousel image ${index}`} />
-                            ))}
-                        </Carousel>
-                    </div>
-                </div>
-                {/* dettagli prodotto */}
-                <div>
-                    <h1>{props.title}</h1>
-                    <Rating>
-                        <Rating.Star />
-                        <Rating.Star />
-                        <Rating.Star />
-                        <Rating.Star />
-                        <Rating.Star filled={false} />
-                    </Rating>
-                    <p>{props.price}</p>
-                    <SpecialButton content="add to Cart" />
-                </div>
-            </div>
-            <Accordion>
-      <Accordion.Panel>
-        <Accordion.Title>What is Flowbite?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons,
-            dropdowns, modals, navbars, and more.
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            Check out this guide to learn how to&nbsp;
-            <a
-              href="https://flowbite.com/docs/getting-started/introduction/"
-              className="text-cyan-600 hover:underline dark:text-cyan-500"
+          {/* Carosello verticale delle immagini */}
+          <div>
+            <Swiper
+              spaceBetween={10}
+              navigation={true}
+              thumbs={{ swiper: thumbsSwiper }}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="mySwiper2"
             >
-              get started&nbsp;
-            </a>
-            and start developing websites even faster with components on top of Tailwind CSS.
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>Is there a Figma file available?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Flowbite is first conceptualized and designed using the Figma software so everything you see in the library
-            has a design equivalent in our Figma file.
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            Check out the
-            <a href="https://flowbite.com/figma/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-              Figma design system
-            </a>
-            based on the utility classes from Tailwind CSS and components from Flowbite.
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>What are the differences between Flowbite and Tailwind UI?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            The main difference is that the core components from Flowbite are open source under the MIT license, whereas
-            Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
-            components, whereas Tailwind UI offers sections of pages.
-          </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
-            technical reason stopping you from using the best of two worlds.
-          </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-            <li>
-              <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                Flowbite Pro
-              </a>
-            </li>
-            <li>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="Slide 1" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="Slide 2" />
+              </SwiperSlide>
+              {props.immagini.map((product, index) => (
+                <SwiperSlide key={index}>
+                  <img src={product.img1} alt={`carousel image ${index}`} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          {/* Carosello grande */}
+          <div>
+            <Swiper
+              onSwiper={(swiper) => setThumbsSwiper(swiper)}
+              spaceBetween={10}
+              slidesPerView={4}
+              freeMode={true}
+              watchSlidesProgress={true}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="mySwiper">
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+              </SwiperSlide>
+              {props.immagini.map((product, index) => (
+                <SwiperSlide key={index}><img src={product.img1} alt={`carousel image ${index}`} /></SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+
+        {/* dettagli prodotto */}
+        <div>
+          <h1>{props.title}</h1>
+          <Rating>
+            <Rating.Star />
+            <Rating.Star />
+            <Rating.Star />
+            <Rating.Star />
+            <Rating.Star filled={false} />
+          </Rating>
+          <p>{props.price}</p>
+          <SpecialButton content="add to Cart" />
+        </div>
+      </div>
+      <Accordion>
+        <Accordion.Panel>
+          <Accordion.Title>What is Flowbite?</Accordion.Title>
+          <Accordion.Content>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons,
+              dropdowns, modals, navbars, and more.
+            </p>
+            <p className="text-gray-500 dark:text-gray-400">
+              Check out this guide to learn how to&nbsp;
               <a
-                href="https://tailwindui.com/"
-                rel="nofollow"
+                href="https://flowbite.com/docs/getting-started/introduction/"
                 className="text-cyan-600 hover:underline dark:text-cyan-500"
               >
-                Tailwind UI
+                get started&nbsp;
               </a>
-            </li>
-          </ul>
-        </Accordion.Content>
-      </Accordion.Panel>
-    </Accordion>
-            <Footer />
-        </div>
-    );
+              and start developing websites even faster with components on top of Tailwind CSS.
+            </p>
+          </Accordion.Content>
+        </Accordion.Panel>
+        <Accordion.Panel>
+          <Accordion.Title>Is there a Figma file available?</Accordion.Title>
+          <Accordion.Content>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              Flowbite is first conceptualized and designed using the Figma software so everything you see in the library
+              has a design equivalent in our Figma file.
+            </p>
+            <p className="text-gray-500 dark:text-gray-400">
+              Check out the
+              <a href="https://flowbite.com/figma/" className="text-cyan-600 hover:underline dark:text-cyan-500">
+                Figma design system
+              </a>
+              based on the utility classes from Tailwind CSS and components from Flowbite.
+            </p>
+          </Accordion.Content>
+        </Accordion.Panel>
+        <Accordion.Panel>
+          <Accordion.Title>What are the differences between Flowbite and Tailwind UI?</Accordion.Title>
+          <Accordion.Content>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              The main difference is that the core components from Flowbite are open source under the MIT license, whereas
+              Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
+              components, whereas Tailwind UI offers sections of pages.
+            </p>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
+              technical reason stopping you from using the best of two worlds.
+            </p>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+            <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+              <li>
+                <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
+                  Flowbite Pro
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://tailwindui.com/"
+                  rel="nofollow"
+                  className="text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Tailwind UI
+                </a>
+              </li>
+            </ul>
+          </Accordion.Content>
+        </Accordion.Panel>
+      </Accordion>
+      <Footer />
+    </div>
+  );
 }
