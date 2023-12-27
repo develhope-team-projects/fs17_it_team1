2e0bea12f05c2ab55156b0c5b25ef1c85a6558a1
stefan <137123241+Stef533@users.noreply.github.com>
2023-12-27 14:18:43 +0100

    added filters button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Store} from "./store/Store";
+import Store from "./store/Store";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 
--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,85 +1,226 @@
 import CardStd from "../product/CardStd";
 import Header from "../shared/Header";
+import { useState } from "react";
+
 const text = {
-  title: "Prodotto",
+  title: "Jack Daniel's Tennessee Whiskey Old N. 7 Brand 1Litro",
   rating: 4,
   img: "",
   price: 34.99
-
-
 }
-export function Store (){ 
-   return(
 
-    
+const text2 = {
+  title: "Bulleit Bourbon Frontier Whiskey 70cl",
+  rating: 5,
+  img: "",
+  price:
+   14.99
+}
+const text3 = {
+  title: "Jim Beam Bourbon White Label 1Litro",
+  rating: 43,
+  img: "",
+  price: 15.99
+}
+
+const text4 = {
+  title: "Whisky Akashi Red 50cl",
+  rating: 5,
+  img: "",
+  price: 24.99
+}
+
+const text5 = {
+  title: "1974 Barolo DOCG - Guasti",
+  rating: 5,
+  img: "",
+  price: 59.99
+}
+
+const text6 = {
+  title: "2010 Brunello di Montalcino DOCG - Piombaia",
+  rating: 2,
+  img: "",
+  price: 49.99
+}
+
+const text7 = {
+  title: "Merlot Marca Trevigiana IGP - Baron Winery",
+  rating: 3,
+  img: "",
+  price: 29.99
+}
+
+const text8 = {
+  title: "1996 Tegolaia - Vila Travignoli",
+  rating: 1,
+  img: "",
+  price: 84.99
+}
+
+const text9 = {
+  title: "Vodka Nikka Coffey - Nikka [0.70 Lt, Astucciato]",
+  rating: 5,
+  img: "",
+  price: 249.99
+}
+
+const text10 = {
+  title: "Vodka Beluga Celebration - Mariinsk Distillery [0.70 Lt]",
+  rating: 4,
+  img: "",
+  price: 44.99
+}
+
+const text11 = {
+  title: "Vodka Beluga Allure Pelle - Beluga [0.70 Lt]",
+  rating: 4,
+  img: "",
+  price: 149.99
+}
+
+const text12 = {
+  title: "Vodka Eiko [0.70 Lt]",
+  rating: 3,
+  img: "",
+  price: 99.99
+}
+
+const Store = () => {
+  const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
+
+  const toggleFilter = () => {
+    setIsFilterOpen(!isFilterOpen);
+  };
+
+   return(
     <div>
       <div className="header">
             <Header/>
       </div>
-      <div className="vuoto h-40"></div>
-      <div className="filters">
-      <div className="flex items-center justify-right p-4">
-  <button id="dropdownDefault" data-dropdown-toggle="dropdown"
-    className="text-black hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-yellow-500"
-    type="button">
-    Filter by category
-    <svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-    </svg>
-  </button>
 
-  <div id="dropdown" className="z-10 hidden w-56 p-3 bg-600 rounded-lg shadow dark:bg-gray-700">
-    <h6 className="mb-3 text-sm font-medium text-gray-900 dark:text-white">
-      Category
-    </h6>
-    <ul className="space-y-2 text-sm" aria-labelledby="dropdownDefault">
-      <li className="flex items-center">
-        <input id="apple" type="checkbox" value=""
-          className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-
-        <label htmlFor="apple" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-          Whisky (56)
-        </label>
-      </li>
-
-      <li className="flex items-center">
-        <input id="fitbit" type="checkbox" value=""
-          className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-
-        <label htmlFor="fitbit" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-          Vodka (56)
-        </label>
-      </li>
-
-      <li className="flex items-center">
-        <input id="dell" type="checkbox" value=""
-          className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-
-        <label htmlFor="dell" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-          Bjorn (56)
-        </label>
-      </li>
-    </ul>
-  </div>
-</div>
+      <div className="vuoto
+      h-40">
       </div>
-    <div className="cards mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-3 ml-4 mr-4 mt-11">
+
+      <div className="container
+       mx-4
+       max-w-lg">
+
+      <button
+        onClick={toggleFilter}
+        className="
+        bg-yellow-500
+        hover:bg-orange-50
+        text-white
+        hover:text-black
+        hover:border-2
+        border-yellow-500
+        font-bold
+        py-2
+        px-4
+        rounded"
+      >
+        Filtra
+      </button>
+
+
+{isFilterOpen && (
+  <div className="
+      bg-white
+      shadow-md
+      mt-2
+      p-4
+      rounded-2xl
+      shadow-2xl ">
+  <label className="
+      block
+      text-gray-700
+      text-sm
+      font-bold
+      mb-2 " >
+        Prezzo:
+    <select className="
+        form-select
+        mt-1
+        block
+        w-full
+        rounded-l-lg">
+          <option> 14.99</option>
+          <option>24.99</option>
+          <option>39.99</option>
+          <option>59.99</option>
+     </select>
+  </label>
+
+
+    <label className="
+    block
+    text-gray-700
+    text-sm
+    font-bold
+    mb-2 ">
+    Categorie:
+      <select className="
+      form-select
+      mt-1
+      block
+      w-full
+      rounded-l-lg">
+        <option>Vini</option>
+        <option>Liquori</option>
+        <option>Spumanti</option>
+        <option>Distillati</option>
+      </select>
+    </label>
+
+         
+     <button className="
+    bg-yellow-500
+    hover:bg-orange-50
+    text-white
+    hover:text-black
+    hover:border-2
+    border-yellow-500
+    text-white
+    font-bold
+    py-2
+    px-4
+    rounded
+    mt-4">
+    Applica Filtri
+    </button>
+  </div>
+ )}
+</div>
+
+
+    <div className="cards
+         mx-auto
+         grid grid-cols-1
+         sm:grid-cols-2
+         md:grid-cols-3
+         lg:grid-cols-4
+         gap-3
+         ml-4
+         mr-4
+         mt-11">
+
     <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
-    <CardStd {...text}/>
+    <CardStd {...text2}/>
+    <CardStd {...text3}/>
+    <CardStd {...text4}/>
+    <CardStd {...text5}/>
+    <CardStd {...text6}/>
+    <CardStd {...text7}/>
+    <CardStd {...text8}/>
+    <CardStd {...text9}/>
+    <CardStd {...text10}/>
+    <CardStd {...text11}/>
+    <CardStd {...text12}/>
     </div>
-    </div>
-  )
+  </div>
+
+ );
 }
-
-
+ export default Store
