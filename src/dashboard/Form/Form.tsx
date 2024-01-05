096b2006be503934cc82c2071aa933a75af9b7dd
Adolfo-97 <adolfogomez@live.it>
2024-01-05 12:01:27 +0100

    form inputs done, need to create the post function

--- src/dashboard/Form/Form.tsx
+++ src/dashboard/Form/Form.tsx
@@ -5,27 +5,69 @@ import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 export function Form() {
   return (
     <form className="flex max-w-md flex-col gap-4">
+      {/* EMAIL */}
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="email1" value="Your email" />
+          <Label htmlFor="email1" value="Account email" />
         </div>
         <TextInput
           id="email1"
           type="email"
-          placeholder="name@flowbite.com"
+          placeholder="email@email.com"
           required
         />
       </div>
+      {/* First name */}
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="password1" value="Your password" />
+          <Label htmlFor="email1" value="Account first name" />
         </div>
-        <TextInput id="password1" type="password" required />
+        <TextInput
+          id="firstname"
+          type="text"
+          placeholder="first name"
+          required
+        />
       </div>
-      <div className="flex items-center gap-2">
-        <Checkbox id="remember" />
-        <Label htmlFor="remember">Remember me</Label>
+      {/* Last name */}
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="email1" value="Account last name" />
+        </div>
+        <TextInput id="lastname" type="text" placeholder="last name" required />
       </div>
+      {/* Birth day */}
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="email1" value="Account birth day" />
+        </div>
+        <TextInput
+          id="birthday"
+          type="text"
+          placeholder="dd-mm-yyyy"
+          required
+        />
+      </div>
+      {/* Location */}
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="email1" value="Account location" />
+        </div>
+        <TextInput id="location" type="text" placeholder="city" required />
+      </div>
+      {/* Gender */}
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="email1" value="Account gender" />
+        </div>
+        <TextInput
+          id="gender"
+          type="text"
+          placeholder="male or female"
+          required
+        />
+      </div>
+
       <Button type="submit">Submit</Button>
     </form>
   );
