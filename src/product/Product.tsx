a4e517e208ac9c71f27b86cc57e8e431019ebf70
Bjorn Agbalog <bjornlouie98@gmail.com>
2024-01-09 09:50:16 +0100

    fix/ fetchato con il server express

--- src/product/Product.tsx
+++ src/product/Product.tsx
@@ -3,7 +3,7 @@
 
 import { Footer } from "flowbite-react";
 import { SitePathComponent } from "../shared/SitePath";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../shared/Header";
 import { Rating } from "flowbite-react";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -17,20 +17,40 @@ import { Navigation } from "swiper/modules";
 import { Accordion } from "flowbite-react";
 import { NormalButton } from "../shared/NormalButton";
 
-
-type ProductProps = {
+/* type ProductProps = {
   title: string;
   immagini: { img1: string }[];
   price: number;
   discountPrice?:number;
 };
-
-export function Product(props: ProductProps) {
+ */
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPrice?: number;
+  immagini: { img: string }[];
+}
+export function Product({ match }: any) {
   /*  const [img, setImg] = useState<string | undefined>(undefined);
 
     const handleClick = (image: string) => {
         setImg(image);
     }; */
+
+  const productId = match.params.id;
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    // Chiamata API per ottenere i dettagli del prodotto dal server
+    fetch(`http://localhost:3001/products/${productId}`)
+      .then((response) => response.json())
+      .then((data: Product) => setProduct(data))
+      .catch((error) =>
+        console.error("Errore nel recupero dei dettagli del prodotto:", error)
+      );
+  }, [productId]);
 
   return (
     <div>
@@ -42,10 +62,11 @@ export function Product(props: ProductProps) {
           thirdTitle="product"
         />
       </div>
-      <div className="flex items-center my-10">
-        <div className="w-8/12">
-          {/* Carosello vertifcali delli immagini */}
-          {/* <div>
+      {product ? (
+        <div className="flex items-center my-10">
+          <div className="w-8/12">
+            {/* Carosello vertifcali delli immagini */}
+            {/* <div>
               {props.immagini.map((product, index)=> (
                 <button onClick={() => handleClick(product.img1)} >
                   <img
@@ -57,45 +78,48 @@ export function Product(props: ProductProps) {
               ))}
             </div> */}
 
-          <Swiper
-            rewind={true}
-            navigation={true}
-            modules={[Navigation]}
-            className="w-9/12"
-          >
-            {props.immagini.map((product, index) => (
-              <SwiperSlide>
-                <img
-                  key={index}
-                  src={product.img1}
-                  alt={`carousel image ${index}`}
-                  className="w-full h-full max-h-fit"
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-        {/* dettagli prodotto */}
-        <div className="w-full h-full mr-32">
-          <h1 className="text-5xl pb-3 font-semibold">{props.title}</h1>
-          <Rating>
-            <Rating.Star />
-            <Rating.Star />
-            <Rating.Star />
-            <Rating.Star />
-            <Rating.Star filled={false} />
-          </Rating>
-          <div className="flex items-center pt-5 pb-8">
-            <p className="text-3xl pr-3">{props.price}$</p>
-            <p className="text-xl line-through font-thin text-gray-700">
-              {props.discountPrice}$
-            </p>
+            <Swiper
+              rewind={true}
+              navigation={true}
+              modules={[Navigation]}
+              className="w-9/12"
+            >
+              {product.immagini.map((product, index) => (
+                <SwiperSlide>
+                  <img
+                    key={index}
+                    src={product.img}
+                    alt={`carousel image ${index}`}
+                    className="w-full h-full max-h-fit"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
+          {/* dettagli prodotto */}
+          <div className="w-full h-full mr-32">
+            <h1 className="text-5xl pb-3 font-semibold">{product.title}</h1>
+            <Rating>
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star filled={false} />
+            </Rating>
+            <div className="flex items-center pt-5 pb-8">
+              <p className="text-3xl pr-3">{product.price}$</p>
+              <p className="text-xl line-through font-thin text-gray-700">
+                {product.discountPrice}$
+              </p>
+            </div>
 
-          <NormalButton content="Add to cart" customstyle="w-full h-11" />
+            <NormalButton content="Add to cart" customstyle="w-full h-11" />
+          </div>
         </div>
-      </div>
-      
+      ) : (
+        <p>...caricamento</p>
+      )}
+
       <Footer />
     </div>
   );
