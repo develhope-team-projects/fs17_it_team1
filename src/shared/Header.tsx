baf70178d74138cfaa9feea004f214b799bd04d9
Eno-Mario <pisellienomario@gmail.com>
2024-01-17 19:38:11 +0100

    fix: logo header

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -49,7 +49,7 @@ export default function Header() {
             <Navbar.Brand>
               <img
                 src="/src/assets/logo_white-03.svg"
-                className="mr-3 h-6 sm:h-9"
+                className="mr-44 h-6 sm:h-9"
                 alt="LOKI Logo"
               />
             </Navbar.Brand>
