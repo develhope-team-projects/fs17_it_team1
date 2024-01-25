8c1539360857ec8eee35e9550034840b2a60ea7a
Eno-Mario <pisellienomario@gmail.com>
2024-01-25 02:26:52 +0100

    fix: quantity selector

--- src/ecommerce/Cart/Cart.tsx
+++ src/ecommerce/Cart/Cart.tsx
@@ -34,31 +34,47 @@ function Cart() {
   };
 
   async function changeQuantity(
-    event: ChangeEvent<HTMLSelectElement>,
-    id: number
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    id: number,
+    quantity: number
   ) {
-    console.log(event.target.value);
+    console.log(event.currentTarget.id);
+    console.log(`increment-button`);
 
-    const quantity = { quantity: event.target.value };
-    if (Number(event.target.value) > 0) {
+    console.log(event.currentTarget.id == `increment-button`);
+
+    if (event.currentTarget.id == `increment-button`) {
       const response = await fetch(`http://localhost:3001/cart/${id}`, {
         method: "PATCH", // *GET, POST, PUT, DELETE, etc.
         headers: {
           "Content-Type": "application/json",
           // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: JSON.stringify(quantity), // body data type must match "Content-Type" header
+        body: JSON.stringify({ quantity: quantity + 1 }), // body data type must match "Content-Type" header
       });
 
       onFetchData();
 
-      event.target.options[1].selected = true;
-
       return response.json();
     } else {
-      deleteCartRow(id);
-      setCartData((d: any) => d.filter((del: any) => del.id !== id));
-      return;
+      if (quantity === 1) {
+        deleteCartRow(id);
+        setCartData((d: any) => d.filter((del: any) => del.id !== id));
+        return;
+      } else {
+        const response = await fetch(`http://localhost:3001/cart/${id}`, {
+          method: "PATCH", // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: JSON.stringify({ quantity: quantity - 1 }), // body data type must match "Content-Type" header
+        });
+
+        onFetchData();
+
+        return response.json();
+      }
     }
   }
 
@@ -81,16 +97,81 @@ function Cart() {
             <p className="text-base font-black leading-none text-gray-800">
               {product && product[0].name}
             </p>
-            <select
-              className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none"
-              onChange={(event) => changeQuantity(event, Number(props.id))}
-            >
-              <option>{props.quantity - 1}</option>
-              <option id="selected" selected>
-                {props.quantity}
-              </option>
-              <option>{props.quantity + 1}</option>
-            </select>
+            <form className="max-w-xs mx-auto">
+              <label
+                htmlFor="quantity-input"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Choose quantity:
+              </label>
+              <div className="relative flex items-center max-w-[8rem]">
+                <button
+                  type="button"
+                  id="decrement-button"
+                  data-input-counter-decrement="quantity-input"
+                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                  onClick={(event) =>
+                    changeQuantity(event, Number(props.id), props.quantity)
+                  }
+                >
+                  <svg
+                    className="w-3 h-3 text-gray-900 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 18 2"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 1h16"
+                    />
+                  </svg>
+                </button>
+                <input
+                  type="text"
+                  id="quantity-input"
+                  data-input-counter
+                  aria-describedby="helper-text-explanation"
+                  className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={props.quantity}
+                  required
+                />
+                <button
+                  type="button"
+                  id="increment-button"
+                  onClick={(event) =>
+                    changeQuantity(event, Number(props.id), props.quantity)
+                  }
+                  data-input-counter-increment="quantity-input"
+                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                >
+                  <svg
+                    className="w-3 h-3 text-gray-900 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 18 18"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M9 1v16M1 9h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <p
+                id="helper-text-explanation"
+                className="mt-2 text-sm text-gray-500 dark:text-gray-400"
+              >
+                Please select a 5 digit number from 0 to 9.
+              </p>
+            </form>
           </div>
 
           <div className="flex items-center justify-between pt-5 pr-6">
