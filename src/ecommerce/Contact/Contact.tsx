f5ea06a56944895e38b25acf814ea833d601cdc9
stefan <137123241+Stef533@users.noreply.github.com>
2024-01-29 18:11:24 +0100

    contact funziona

--- src/ecommerce/Contact/Contact.tsx
+++ src/ecommerce/Contact/Contact.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Button, Label, TextInput } from 'flowbite-react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const [formData, setFormData] = useState({
@@ -10,14 +10,29 @@ function Contact() {
 
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleChange = (e: { target: { name: string; value: string; }; }) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) : void => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    console.log('Dati del form inviati:', formData);
+
+    emailjs
+      .sendForm(
+        "service_ys6skm7",
+        "template_4a60etk",
+        e.currentTarget,
+        "8KonMB_HAwGZ1NymP"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        });
+
     setFormData({
       name: '',
       email: '',
@@ -25,6 +40,7 @@ function Contact() {
     });
     setIsSubmitted(true);
   };
+
 
   return (
     <div className="
@@ -38,23 +54,24 @@ function Contact() {
       mb-20
       md:rounded-lg
     ">
-          <div className="w-4/5 flex flex-col items-start md:px-10 py-10 md:w-3/5">
+      <div className="w-4/5 flex flex-col items-start md:px-10 py-10 md:w-3/5">
         <h5 className="text-4xl font-bold text-gray-900 dark:text-white pb-5">
           Contact US
         </h5>
         <p className="font-normal text-gray-700 dark:text-gray-400">
-        Send a message to our liquor store for inquiries, product requests, or information on promotions. We're here to assist you and make your alcoholic beverage shopping experience unique and memorable. Fill out the form below, and you'll soon be in touch with our knowledgeable team </p>      </div>
+          Send a message to our liquor store for inquiries, product requests, or information on promotions. We're here to assist you and make your alcoholic beverage shopping experience unique and memorable. Fill out the form below, and you'll soon be in touch with our knowledgeable team.
+        </p>
+      </div>
       {isSubmitted ? (
-        <div className="p-8 w-full max-w-md mx-auto  rounded-md ">
+        <div className="p-8 w-full max-w-md mx-auto rounded-md ">
           <p className="text-green-600">Messaggio inviato con successo!</p>
-
         </div>
       ) : (
         <form onSubmit={handleSubmit} className="p-8 w-full max-w-md mx-auto rounded-md justify-end">
-          <Label htmlFor="name" className="block text-sm font-medium text-gray-700">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
             Nome
-          </Label>
-          <TextInput
+          </label>
+          <input
             type="text"
             id="name"
             name="name"
@@ -69,10 +86,10 @@ function Contact() {
             text-white" 
           />
 
-          <Label htmlFor="email" className="block mt-4 text-sm font-medium text-gray-700">
+          <label htmlFor="email" className="block mt-4 text-sm font-medium text-gray-700">
             Email
-          </Label>
-          <TextInput
+          </label>
+          <input
             type="email"
             id="email"
             name="email"
@@ -87,9 +104,9 @@ function Contact() {
             text-white"
           />
 
-          <Label htmlFor="message" className="block mt-4 text-sm font-medium text-gray-700">
+          <label htmlFor="message" className="block mt-4 text-sm font-medium text-gray-700">
             Message
-          </Label>
+          </label>
           <textarea
             id="message"
             name="message"
@@ -100,14 +117,13 @@ function Contact() {
           />
 
           <div className="mt-6">
-            <Button type="submit" className="w-full text-white p-2 rounded-md">
+            <button type="submit" className="w-full text-white p-2 rounded-md bg-blue-500 hover:bg-blue-700">
               Send Message
-            </Button>
+            </button>
           </div>
         </form>
       )}
     </div>
   );
 }
-
 export default Contact;
